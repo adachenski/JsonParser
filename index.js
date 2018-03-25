d36434601d9cdf87515b021c82435c1165a2a7bb
nasko <adachenski@aol.com>
2018-03-24 22:03:32 -0700

    Second row is showing

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ $(document).ready(function () {
                 //console.log(evt.target.result)
                 var json2 = evt.target.result;
                 var json = JSON.parse(json2);
+                //console.log(json);
                 console.log(json2.length)
                 console.log(json.length)
-              
+
                 printOnScreen(json);
                 //initialTable(evt.target.result);
                 // document.getElementById('byte_range').textContent = 
@@ -46,22 +47,62 @@ $(document).ready(function () {
 
 
     function printOnScreen(json) {
-        //console.log(json)
-        var tr;
+        console.log(json)
+        var tr, tr2;
+        var firstRowParams;
+        var row2TD;
+        var row2Parms;
+        var card;
+        var anchor;
         var param = [];
         var tableCss = "style='border:1px solid black; padding:5px'";
         for (var i = 0; i < json.length; i++) {
             param.push(json[i].parameters);
             // console.log(param[i]);
             tr = $('<tr/>');
+           
+            tr2 = $('<tr/>', {
+                class: 'collapse',
+                id: 'param_' + i
+            });
+            card = $('<div/>');
+            firstRowParams = $('<td/>');
+            anchor = $('<a/>')
+                // .css({'border':'1px solid black', 'padding':'5px'})
+                .attr('data-toggle', 'collapse')
+                .attr('href', '#param_' + i)
+                .attr('aria-expanded', 'false')
+                .attr('data-toggle', 'collapse')
+                .attr('aria-controls', '#param_' + i)
+                .attr('class', 'btn btn-primary')
+                .text('Params');
+
+            row2TD = $('<div/>',{
+                class: 'card card-body'
+            });
+
             tr.append("<td " + tableCss + ">" + json[i].id + "</td>");
             tr.append("<td " + tableCss + ">" + json[i].disabled + "</td>");
             tr.append("<td " + tableCss + ">" + json[i].name + "</td>");
             tr.append("<td " + tableCss + ">" + json[i].description + "</td>");
             tr.append("<td " + tableCss + ">" + json[i].group + "</td>");
-            tr.append("<td>" + json[i].parameters + "</td>");
-            // paramTable(param);
+
+            firstRowParams.append(anchor);
+            tr.append(firstRowParams);
+
+            console.log("empty")
+            //second row
+            for (key in json[i].parameters){
+                row2Parms = $('<td/>').attr('class','col-xs-1').text(key);
+                card.append(row2Parms);
+                console.log( key + ": " + json[i].parameters);
+            }
+
+            row2TD.append(card);
+            tr2.append(row2TD);
+
             $('table').append(tr);
+            $('table').append(tr2);
         }
     };
 
